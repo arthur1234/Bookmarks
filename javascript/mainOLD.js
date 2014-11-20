5bb6a8cabd3e7e97bd51da7d264b76bf7a3307e8
arthur1234 <kidumaikido@gmail.com>
2014-11-20 11:07:06 +0200

    second update

--- javascript/mainOLD.js
+++ javascript/mainOLD.js
@@ -8,7 +8,7 @@ var cmd = (function() {
 	var cmd = {
 
 		// PRIVATE FUNCTION
-		createFullMenu : function (local_data,id, flag) {
+		createFullMenu : function (data,id, flag) {
 
 			var iflast, 
 				doInList, 
@@ -18,34 +18,33 @@ var cmd = (function() {
 			if(flag) line="_";
 			var fullDirList = '<ul id="main_'+id+line+'">';
 
-			for(var ii = 0; ii < local_data.length; ii++) {
+			for(var ii = 0; ii < data.length; ii++) {
 
 				iflast = '';
-				if(ii == local_data.length-1)
+				if(ii == data.length-1)
 					iflast = ' last';
 
-				// IF local_data CURRENT ELEMENT (json.js) HAVE CHILDS ELEMENTS ->> CREATE TREE
-				if( local_data[ii].childs.length > 0 && !flag ) {
-					fullDirList += '<li id="'+id+ii+'" class="has-sub'+iflast+'"><a href="'+local_data[ii].url+'">'+
-					local_data[ii].dirName+
+				// IF DATA CURRENT ELEMENT (json.js) HAVE CHILDS ELEMENTS ->> CREATE TREE
+				if( data[ii].childs.length > 0 && !flag ) {
+					fullDirList += '<li id="'+id+ii+'" class="has-sub'+iflast+'"><a href="'+data[ii].url+'">'+
+					data[ii].dirName+
 					'</a>';
-					var callbackToGetHtmlString = this.createFullMenu(local_data[ii].childs,id+ii+'_');
+					var callbackToGetHtmlString = this.createFullMenu(data[ii].childs,id+ii+'_');
 					fullDirList += callbackToGetHtmlString;	
 
 				} else {
 					if(!flag) {
-						fullDirList += '<li id="'+id+ii+'" class="inList'+iflast+'"><a href="'+local_data[ii].url+'">'+
-						local_data[ii].dirName+
+						fullDirList += '<li id="'+id+ii+'" class="inList'+iflast+'"><a href="'+data[ii].url+'">'+
+						data[ii].dirName+
 						'</a>';
 					} else {
-						// createSecondMenu  :
-
-						/*if(local_data[ii].childs.length > 0) 
+						
+						if(data[ii].childs.length > 0) 
 							span = '<span class="submit"   onmouseover="cmd.preFilSelect(\''+id+ii+'_\')"></span>';	
 						fullDirList += '<li id="'+id+ii+'_" onclick="console.log(\''+id+ii+'_\')" class="inList'+iflast+
 						'">'+span+
-						local_data[ii].dirName;
-						console.log('cmd.preFilSelect(\''+id+ii+'_\')');*/
+						data[ii].dirName;
+						console.log('cmd.preFilSelect(\''+id+ii+'_\')');
 					}
 				}
 
@@ -54,44 +53,6 @@ var cmd = (function() {
 
 			fullDirList += '</ul>';
 			return fullDirList;
-		},
-		
-		createSecondMenu : function(local_data, id, flag, num){
-			// num is counter of parent 
-			/*	console.log(local_data);
-				console.log(id+' == '+flag.toString());
-			*/
-
-			// put elements in select box
-			num++;
-			var select = '<select id="'+id+'_'+num+'" onchange="cmd.menuSelected(this.id, '+num+');">';
-			for(var ii = 0; ii < local_data.length; ii++) {
-				if( local_data[ii].childs.length > 0 ) {
-					select += '<option value="'+ii+'">'+local_data[ii].dirName+'</option>';
-
-				} else {
-					select += '<option value="'+ii+'">'+local_data[ii].dirName+'</option>';
-				}
-			}
-			select += '</select>';
-			//<div id="putHere_'+num+'"></div>
-			return select;
-		},
-
-		menuSelected : function(id, num) {
-			 var index = document.getElementById(id).value;
-			 var newItem = document.createElement("div")
-			 newItem.id='putHere_'+num;
-		    var textnode = document.createTextNode("Water")
-		    newItem.appendChild(textnode)
-
-
-		     var list = document.getElementById("createDirWindow")
-
-		     list.insertBefore(newItem, list.childNodes[0]);
-
-			 cmd.filSelect(data[index].childs, 'inListPosition_'+num, 'putHere_'+num, num);
-			  console.log(id+' -- '+data[index].childs.length);
 		},
 
 		show : function() {
@@ -102,13 +63,12 @@ var cmd = (function() {
 			
 		},
 
-		filSelect : function (local_data, idBase, idElement, num) {
+		filSelect : function (data, idBase) {
 			
-			cmd.el(idElement).innerHTML = 
-				cmd.createSecondMenu(local_data, idBase, true, num);
+			cmd.el('putHere').innerHTML = 
+				cmd.createFullMenu(data, idBase, true);
 		},
-
-	/*	preFilSelect : function (id) {
+		preFilSelect : function (id) {
 			var index = id.charAt(id.length - 2);
 
 			var deepness = (id.match(/_/g) || []).length;
@@ -117,8 +77,7 @@ var cmd = (function() {
 			// разложить див на координаты в массиве ..for 
 
 			cmd.filSelect(data[index].childs, id);
-		},*/
-
+		},
 		el :  function(id) {
 			// shortcut of getElementById
 			return document.getElementById(id);		
@@ -174,7 +133,6 @@ var cmd = (function() {
 	return {
 		preFilSelect : cmd.preFilSelect,
 		filSelect    : cmd.filSelect,
-		menuSelected : cmd.menuSelected,
 		show         : cmd.show,
 		el      	 : cmd.el,
 		onclick 	 : cmd.onclick,
@@ -198,10 +156,10 @@ window.onload = function(){
 	cmd.el('createNew').onmouseover     = function(){ cmd.focus('createNew') };
 	cmd.el('withURL').onmouseover       = function(){ cmd.focus('withURL') };
 
-	cmd.el('positionInArrObjects').onmouseover     =  function(){ cmd.filSelect(data, 'inListPosition', 'putHere', 0)};
+	//cmd.el('positionInArrObjects').onmouseover     =  function(){ cmd.filSelect(data, 'inList')};
 
 
-	//cmd.el('createDir').onmouseleave = function(){ cmd.save('createDir') };
+	cmd.el('createDir').onmouseleave = function(){ cmd.save('createDir') };
 };
 
 
